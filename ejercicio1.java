cdc7c26b4db4c5817f335836cbe1688d04f1990b
Daniel <danielbritonegrin@gmail.com>
2023-11-27 20:11:12 +0000

    Práctica 1 Ejercicio 2 sin arreglar

---
+++ ejercicio1.java
@@ -0,0 +1,10 @@
+public class ejercicio1 {
+    public static void calculatabla(int a){
+        int valor;
+        for (int i=1; i<=10;i++){
+                valor = a * i;
+                valor = valor+1;
+                System.out.println(a + " x " + i + " = " + valor);
+        }
+    }
+}
