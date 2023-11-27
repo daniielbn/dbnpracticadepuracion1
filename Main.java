d6881a759440da16dbaf68205c5393ef38538d5b
Daniel <danielbritonegrin@gmail.com>
2023-11-27 20:10:55 +0000

    Función Main del programa

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int x = 5;
+        int y = 2;
+        ejercicio1.calculatabla(x);
+        ejercicio2.mostrartablashasta(x);
+        ejercicio3.mostrartablasparcialmente(x, y);
+    }
+}
