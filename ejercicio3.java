043502ce626d586185ff03ed23a0d56db8fd8b43
Daniel <danielbritonegrin@gmail.com>
2023-11-27 20:14:58 +0000

    Práctica 1 Ejercicio 3 arreglado

--- ejercicio3.java
+++ ejercicio3.java
@@ -2,10 +2,10 @@ public class ejercicio3 {
     public static void mostrartablasparcialmente(int a, int b){
         int valor;
         for (int i=1; i<=a; i++){
-            for (int j=1; j<=a;j++){
+            for (int j=1; j<=b;j++){
                 valor = i * j;
-                valor = valor+1;
-                System.out.println(i + " * " + j + " = "+ (valor+b));
+                // valor = valor+1;
+                System.out.println(i + " * " + j + " = "+ (valor/*+b*/));
             }
             System.out.println("--------");
         }
