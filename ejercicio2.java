979253168951cd94bd85c5dbe283f2bf28635bff
Daniel <danielbritonegrin@gmail.com>
2023-11-27 20:14:51 +0000

    Práctica 1 Ejercicio 2 arreglado

--- ejercicio2.java
+++ ejercicio2.java
@@ -2,9 +2,9 @@ public class ejercicio2 {
     public static void mostrartablashasta(int a){
         int valor;
         for (int i=1; i<=a; i++){
-            for (int j=1; j<=a;j++){
+            for (int j=1; j<=10;j++){
                 valor = i * j;
-                valor = valor+1;
+                // valor = valor+1;
                 System.out.println(i + " * " + j + " = "+ valor);
             }
             System.out.println("--------");
